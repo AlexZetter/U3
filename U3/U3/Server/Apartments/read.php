a0388ec6dbc92c9fbc3c6be297644b2040d005cd
Alex <zetterstrom.alexander@gmail.com>
2021-11-16 13:46:41 +0100

    added functions page, added read apartments code

---
+++ U3/U3/Server/Apartments/read.php
@@ -0,0 +1,21 @@
+<?php
+require_once "../functions.php";
+$allApartments = loadJson("../databaseApartments.json");
+
+if (!isset($_GET["id"], $_GET["ids"], $_GET["frstname"])) {
+    echo "<pre>";
+    var_dump($allTenants);
+    echo "</pre>";
+    echo "<pre>";
+    var_dump($allApartments);
+    echo "</pre>";
+} else {
+}
+
+
+
+
+
+
+
+//Parametrar = "id", "ids", "frstname"
