7980e1f758f8d8ea7fd4e683c99f4b6484982d85
Alex <zetterstrom.alexander@gmail.com>
2021-11-17 12:40:39 +0100

    fixed database

--- U3/U3/Server/Apartments/read.php
+++ U3/U3/Server/Apartments/read.php
@@ -1,11 +1,11 @@
 <?php
 require_once "../functions.php";
-$allApartments = loadJson("../databaseApartments.json");
+$database = loadJson("../database.json");
+
+$allApartments = $database["Apartments"];
+
 
 if (!isset($_GET["id"], $_GET["ids"], $_GET["frstname"])) {
-    echo "<pre>";
-    var_dump($allTenants);
-    echo "</pre>";
     echo "<pre>";
     var_dump($allApartments);
     echo "</pre>";
@@ -14,8 +14,61 @@ if (!isset($_GET["id"], $_GET["ids"], $_GET["frstname"])) {
 
 
 
+?>
 
 
 
+
+
+<?php
 
 //Parametrar = "id", "ids", "frstname", limit, include (int eller bool)
+
+
+$requestMethod = $_SERVER["REQUEST_METHOD"];
+
+//limit
+if ($requestMethod == "GET") {
+    if (isset($_GET["n"])) {
+
+        $users = loadJson("database.json");
+        $returnUsers = array_slice($users, 0, $_GET["n"]);
+    }
+
+    //id
+    if (isset($_GET["id"])) {
+        $users = loadJson("database.json");
+        foreach ($users as $key => $user) {
+            if ($user["id"] == $_GET["id"]) {
+                sendJson($users[$key]);
+            }
+        }
+    }
+
+    //ids
+    if (isset($_GET["ids"])) {
+        //IDS
+        $users = loadJson("database.json");
+        $ids = explode(",", $_GET["ids"]);
+        $arrayOfUsers = [];
+        foreach ($users as $user) {
+            if (in_array($user["id"], $ids)) {
+                $arrayOfUsers[] = $user;
+            }
+        }
+        sendJson($arrayOfUsers);
+    }
+
+
+    //first_name
+
+    if (isset($_GET["id"])) {
+        $users = loadJson("database.json");
+        foreach ($users as $key => $user) {
+            if ($user["first_name"] == $_GET["first_name"]) {
+                sendJson($users[$key]);
+            }
+        }
+    }
+}
+?>
