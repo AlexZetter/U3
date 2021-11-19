1ecf9f79a0839c8d533bc12b27db2f0727a96d01
mercieakerblom <mercie.akerblom@hotmail.com>
2021-11-19 09:25:10 +0100

    lagt till create i Tenants

--- U3/U3/Server/Tenants/create.php
+++ U3/U3/Server/Tenants/create.php
@@ -5,45 +5,42 @@ $inputData = loadJson("php://input");
 $requestData = $_SERVER["REQUEST_METHOD"];
 $contentType = $_SERVER["CONTENT_TYPE"];
 
-
 if (isset($inputData)){
-    if ($requestData === "POST"){
-        if($contentType === "application/json"){
-            $database = loadJson("../database.json");
-            $allTenants = $database["Tenants"];
+    if (!empty($inputData["first_name"]) && !empty($inputData["last_name"]) && !empty($inputData["email"])){
+        if ($requestData === "POST"){
+            if($contentType === "application/json"){
+                $database = loadJson("../database.json");
+                $allTenants = $database["Tenants"];
 
+                $highestID = max((array_column($database["Tenants"], "id")));
+                $nextID = $highestID + 1;
+                $firstName = $inputData["first_name"];
+                $lastName = $inputData["last_name"];
+                $email = $inputData["email"];
 
-            $highestID = max((array_column($database["Tenants"], "id")));
-            $nextID = $highestID + 1;
-
-            $firstName = $inputData["first_name"];
-            $lastName = $inputData["last_name"];
-            $email = $inputData["email"];
-
-            if(!isset($firstName, $lastName, $email)){
-                sendJson(["message" => "Du måste fylla i alla fält"], 400);
+                $newTenant = [
+                    "id" => $nextID,
+                    "first_name" => $firstName,
+                    "last_name" => $lastName,
+                    "email" => $email,
+                    "tenant_id" => $nextID
+                ];
+                array_push($database["Tenants"], $newTenant);
+                saveJson("../database.json", $database);
+                sendJson([$newTenant], 201);
+                
+            }else {
+                sendJson(["message" => "not json"], 400);
             }
-
-
-            $newTenant = [
-                "id" => $nextID,
-                "street_name" => $firstName,
-                "street_number" => $lastName,
-                "realtor" => $email,
-                "tenant_id" => $nextID
-            ];
-
-
-            array_push($database["Tenants"], $newTenant);
-
-            saveJson("../database.json", $database);
-            sendJson([$newTenant], 201);
-            
-        }else {
-            sendJson(["message" => "bad request!"], 400);
+        } else {
+            sendJson(["message" => "method not allowed!"], 400);
         }
+    } else {
+        sendJson(["message" => "Du har glömt fylla i något av fälten"], 400);
     }
-}
+} else {
+    sendJson(["message" => "isset är TOOOM"], 400);
+} 
 
 
 ?>
